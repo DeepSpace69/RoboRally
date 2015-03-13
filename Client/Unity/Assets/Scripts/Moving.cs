f259159c515a23a904bd4f81f00928c5e07e027c
Keroberos <v.v.avramenko@gmail.com>
2015-03-14 01:00:07 +0200

    Move cube by keyboard

--- Client/Unity/Assets/Scripts/Moving.cs
+++ Client/Unity/Assets/Scripts/Moving.cs
@@ -1,15 +1,26 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class Moving : MonoBehaviour {
+public class Moving : MonoBehaviour
+{
 
-	// Use this for initialization
-	void Start () {
-	
-	}
-	
-	// Update is called once per frame
-	void Update () {
-	
-	}
+    float speed = 3f;
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        var move = new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical"));
+        transform.position += move * speed * Time.deltaTime;
+    }
+
+    void FixedUpdate()
+    {
+
+    }
 }
