4df0d37aa6e85f32fee632e05b47440cf4b2841a
Keroberos <v.v.avramenko@gmail.com>
2015-03-19 10:09:38 +0200

    Move camera & Move by cards
    - First simple script to move camera
    - Move robot by pressing the card

--- Client/Unity/Assets/Scripts/BoardBuilder.cs
+++ Client/Unity/Assets/Scripts/BoardBuilder.cs
@@ -4,15 +4,25 @@ using System.Collections;
 public class BoardBuilder : MonoBehaviour
 {
     public GameObject plate;
+    public GameObject wall;
 
     // Use this for initialization
-    void Start()
+    void Awake()
     {
-        for (int y = 0; y < 5; y++)
+        for (int y = -5; y < 6; y++)
         {
-            for (int x = 0; x < 5; x++)
+            for (int x = -5; x < 6; x++)
             {
-                Instantiate(plate, new Vector3(x, 0, y), Quaternion.identity);
+                if (x == 5 || x == -5 || y == 5 || y == -5)
+                {
+                    Instantiate(wall, new Vector3(x, 0.5f, y), Quaternion.identity);
+
+                }
+                else
+                {
+                    Instantiate(plate, new Vector3(x, 0, y), Quaternion.identity);
+
+                }
             }
         }
     }
---
+++ Client/Unity/Assets/Scripts/MoveRobot.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MoveRobot : MonoBehaviour {
+
+	public void MoveForvard()
+    {
+        var currentPosition = gameObject.transform.position;
+        currentPosition.z++;
+        gameObject.transform.position = currentPosition;
+    }
+}
